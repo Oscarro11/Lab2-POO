486e4ac756f4333d9d9fc36af6f9dda9f495ce16
Oscarro11 <oscarrodrigomelchore@gmail.com>
2025-09-05 21:14:50 -0600

    docs: UML diagram for program created. Pending of completing models, controller and vista of main program

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,8 @@
+import java.nio.charset.StandardCharsets;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.setOut(new java.io.PrintStream(System.out, true, StandardCharsets.UTF_8));
+        System.out.println("\u2764\uFE0F");
     }
 }
